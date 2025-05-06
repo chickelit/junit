e993c69834be28999c2e7d3d8d86220bc3bc2f3b
MARILIA MARQUES DA SILVA <20221D12GR0523@labs.garanhuns.ifpe>
2025-05-06 13:59:47 -0300

    Replace repository files

---
+++ src/test/java/com/example/MyTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MyTest {
+    @Test
+    public void run() {
+        Assert.assertEquals(10, 4 + 6);
+    }    
+}
